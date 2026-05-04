206d745a0a6e46c4e9975aafdf06ba741ed9936e
João Erik da Silva Crisóstomo <joaoerik.dev@gmail.com>
2026-05-04 19:07:34 -0300

    feat: editar entidades

--- app/routes/home.py
+++ app/routes/home.py
@@ -51,7 +51,39 @@ def cliente_novo():
         db.session.commit()
         return redirect(url_for('home.clientes'))
     
-    return render_template('clientes/form.html')
+    return render_template('clientes/form.html', cliente=None)
+
+@bp.route('/clientes/editar/<int:id>', methods=['GET', 'POST'])
+def cliente_editar(id):
+    cliente = Cliente.query.get_or_404(id)
+    if request.method == 'POST':
+        # Atualiza o Endereço
+        if cliente.endereco:
+            cliente.endereco.cep = request.form.get('cep')
+            cliente.endereco.logradouro = request.form.get('logradouro')
+            cliente.endereco.numero = request.form.get('numero')
+            cliente.endereco.bairro = request.form.get('bairro')
+            cliente.endereco.municipio = request.form.get('municipio')
+            cliente.endereco.unidade_federacao = request.form.get('unidade_federacao')
+            cliente.endereco.pais = request.form.get('pais', 'Brasil')
+
+        # Converte strings de data
+        cliente.data_nascimento = datetime.strptime(request.form.get('data_nascimento'), '%Y-%m-%d').date()
+        cliente.vencimento_cnh = datetime.strptime(request.form.get('vencimento_cnh'), '%Y-%m-%d').date()
+
+        # Atualiza o Cliente
+        cliente.nome = request.form.get('nome')
+        cliente.cpf__cnpj = request.form.get('cpf__cnpj')
+        cliente.rg = request.form.get('rg')
+        cliente.email = request.form.get('email')
+        cliente.telefone = request.form.get('telefone')
+        cliente.nome_pai = request.form.get('nome_pai')
+        cliente.nome_mae = request.form.get('nome_mae')
+
+        db.session.commit()
+        return redirect(url_for('home.clientes'))
+        
+    return render_template('clientes/form.html', cliente=cliente)
 
 @bp.route('/veiculos')
 def veiculos():
@@ -86,7 +118,35 @@ def veiculo_novo():
         db.session.commit()
         return redirect(url_for('home.veiculos'))
         
-    return render_template('veiculos/form.html')
+    return render_template('veiculos/form.html', veiculo=None)
+
+@bp.route('/veiculos/editar/<int:id>', methods=['GET', 'POST'])
+def veiculo_editar(id):
+    veiculo = Veiculo.query.get_or_404(id)
+    if request.method == 'POST':
+        veiculo.placa = request.form.get('veiculo_placa', '')
+        veiculo.renavam = request.form.get('veiculo_renavam', '')
+        veiculo.chassis = request.form.get('veiculo_chassi', '')
+        veiculo.marca__modelo = request.form.get('veiculo_marca_modelo', '')
+        veiculo.tipo = request.form.get('veiculo_tipo', '')
+        veiculo.especie = request.form.get('veiculo_especie', '')
+        veiculo.cor = request.form.get('veiculo_cor', '')
+        veiculo.combustivel = request.form.get('veiculo_combustivel', '')
+        veiculo.categoria = request.form.get('veiculo_categoria', '')
+        veiculo.grupo = request.form.get('veiculo_grupo', '')
+        veiculo.ano_fabricacao = request.form.get('veiculo_ano_fab', '')
+        veiculo.ano_modelo = request.form.get('veiculo_ano_mod', '')
+        veiculo.cilindradas = request.form.get('veiculo_cilindradas', '')
+        veiculo.potencia = request.form.get('veiculo_potencia', '')
+        veiculo.passageiros = request.form.get('veiculo_passageiros', '')
+        veiculo.placa_anterior = request.form.get('veiculo_placa_anterior', '')
+        veiculo.proprietario_anterior = request.form.get('veiculo_proprietario_anterior', '')
+        veiculo.cpf__cnpj_anterior = request.form.get('veiculo_cpf_cnpj_anterior', '')
+        
+        db.session.commit()
+        return redirect(url_for('home.veiculos'))
+        
+    return render_template('veiculos/form.html', veiculo=veiculo)
 
 @bp.route('/processos')
 def processos():
@@ -106,4 +166,17 @@ def processo_novo():
         
     clientes = Cliente.query.all()
     veiculos = Veiculo.query.all()
-    return render_template('processos/form.html', clientes=clientes, veiculos=veiculos)
+    return render_template('processos/form.html', clientes=clientes, veiculos=veiculos, processo=None)
+
+@bp.route('/processos/editar/<int:id>', methods=['GET', 'POST'])
+def processo_editar(id):
+    processo = Processo.query.get_or_404(id)
+    if request.method == 'POST':
+        processo.cliente_id = request.form.get('cliente_id') or None
+        processo.veiculo_id = request.form.get('veiculo_id') or None
+        db.session.commit()
+        return redirect(url_for('home.processos'))
+        
+    clientes = Cliente.query.all()
+    veiculos = Veiculo.query.all()
+    return render_template('processos/form.html', clientes=clientes, veiculos=veiculos, processo=processo)
